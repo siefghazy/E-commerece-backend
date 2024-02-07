38265c3b547dc101cd8751f164595a07ed2a9e9a
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-07 18:34:20 +0200

    Update filterQuery.js

--- E-commerce/middleware/filterQuery.js
+++ E-commerce/middleware/filterQuery.js
@@ -1,6 +1,6 @@
-export const filterQuery=()=>{
+export const filterQuery=({fieldName,paramName})=>{
     return(req,res,next)=>{
-        req.query=req.query.where({_id:req.params.id})
+        req.dbQuery=req.dbQuery.where({ [fieldName]:req.params[paramName] })
         next()
     }
 }
