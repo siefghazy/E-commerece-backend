f1f55178b0a22e2e2eafb397884e8b22887ae2f2
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-07 18:31:52 +0200

    Update productRouter.js

--- E-commerce/src/modules/product/routes/productRouter.js
+++ E-commerce/src/modules/product/routes/productRouter.js
@@ -5,10 +5,12 @@ import { queryExecution } from '../../../../middleware/execQuery.js'
 import {validate} from'../../../../middleware/validationMiddleware.js'
 import { addProductSchema,updateProductSchema } from "../../../../validations/productValidation.js";
 import { productModel } from "../models/productModel.js";
+import { pagination } from "../../../../middleware/pagination.js";
+import { populateQuery } from "../../../../middleware/populateMiddleware.js";
 export const productRouter=Router()
 productRouter.route('/')
 .post(validate(addProductSchema),attachAddQuery(productModel),queryExecution())
-.get(attachFindQuery(productModel),queryExecution())
-productRouter.route('/:id')
-.delete(attachDeleteQuery(productModel),filterQuery(),queryExecution())
-.put(validate(updateProductSchema),attachUpdateQuery(productModel),filterQuery(),queryExecution())
+.get(attachFindQuery(productModel),populateQuery('category'),pagination(2),queryExecution())
+productRouter.route('/:productSlug')
+//.delete(attachDeleteQuery(productModel),filterQuery(),queryExecution())
+//.put(validate(updateProductSchema),attachUpdateQuery(productModel),filterQuery(),queryExecution())
