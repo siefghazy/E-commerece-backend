b698f934737532fa26bad42abc9150fe2a6e5065
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-07 18:30:14 +0200

    Update subcatRouter.js

--- E-commerce/src/modules/subcategory/routes/subcatRouter.js
+++ E-commerce/src/modules/subcategory/routes/subcatRouter.js
@@ -3,15 +3,18 @@ import { attachAddQuery,
     attachDeleteQuery,
     attachFindQuery,
     attachUpdateQuery } from '../../../../middleware/attachedQuery.js'
-import { filterQuery } from '../../../../middleware/filterQuery.js'
 import { queryExecution } from '../../../../middleware/execQuery.js'
 import {validate} from'../../../../middleware/validationMiddleware.js'
 import { subCatModel } from "../model/subCatModel.js";
 import { addsubCategorySchema,updateSubCatSchema } from "../../../../validations/subcateValidation.js";
-export const subCatRouter=Router()
+import {subcatFilter}from'../middlware/subcatMiddleware.js'
+import { addSubCategory } from "../middlware/subcatMiddleware.js";
+export const subCatRouter=Router({mergeParams:true})
 subCatRouter.route('/')
 .get(attachFindQuery(subCatModel),queryExecution())
 .post(validate(addsubCategorySchema),attachAddQuery(subCatModel),queryExecution())
-subCatRouter.route('/:id')
-.put(validate(updateSubCatSchema),attachUpdateQuery(subCatModel),filterQuery(),queryExecution())
-.delete(attachDeleteQuery(subCatModel),filterQuery(),queryExecution())
+subCatRouter.route('/:subcategorySlug')
+.post(validate(addsubCategorySchema),addSubCategory,queryExecution())
+.get(attachFindQuery(subCatModel),subcatFilter,queryExecution())
+.put(validate(updateSubCatSchema),attachUpdateQuery(subCatModel),subcatFilter,queryExecution())
+.delete(attachDeleteQuery(subCatModel),subcatFilter,queryExecution())
