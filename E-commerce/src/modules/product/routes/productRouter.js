cd681034712b2e4368fd7589ababaab17d60d35d
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-10 19:23:50 +0200

    Update productRouter.js

--- E-commerce/src/modules/product/routes/productRouter.js
+++ E-commerce/src/modules/product/routes/productRouter.js
@@ -7,10 +7,22 @@ import { addProductSchema,updateProductSchema } from "../../../../validations/pr
 import { productModel } from "../models/productModel.js";
 import { pagination } from "../../../../middleware/pagination.js";
 import { populateQuery } from "../../../../middleware/populateMiddleware.js";
+import { sorting } from "../../../../middleware/sorting.js";
+import { fieldSelection } from "../../../../middleware/fieldSelectionMiddleware.js";
+import { search } from "../../../../middleware/searchMiddleware.js";
+import { filter } from "../../../../middleware/filterMiddlewarae.js";
 export const productRouter=Router()
 productRouter.route('/')
 .post(validate(addProductSchema),attachAddQuery(productModel),queryExecution())
-.get(attachFindQuery(productModel),populateQuery('category'),pagination(2),queryExecution())
+.get(attachFindQuery(productModel),populateQuery('subcategory'),pagination(2),sorting(),fieldSelection(),queryExecution())
 productRouter.route('/:productSlug')
-//.delete(attachDeleteQuery(productModel),filterQuery(),queryExecution())
-//.put(validate(updateProductSchema),attachUpdateQuery(productModel),filterQuery(),queryExecution())
+.delete(attachDeleteQuery(productModel),filterQuery(),queryExecution())
+.put(validate(updateProductSchema),attachUpdateQuery(productModel),filterQuery(),queryExecution())
+productRouter.route('/search')
+.get(attachFindQuery(productModel),search(['title','description']),populateQuery('category'),pagination(2),queryExecution())
+productRouter.route('/fitler')
+.get(attachFindQuery(productModel),filter(),populateQuery('category'),pagination(2),queryExecution())
+productRouter.route('/fieldselection')
+.get(attachFindQuery(productModel),fieldSelection(),populateQuery('category'),pagination(2),queryExecution())
+productRouter.route('/sort')
+.get(attachFindQuery(productModel),sorting(),populateQuery('category'),pagination(2),queryExecution())
