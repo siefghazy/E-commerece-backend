d15ef07c4adde21554030b5789ec87168079b361
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-11 22:52:36 +0200

    Update validationMiddleware.js

--- E-commerce/middleware/validationMiddleware.js
+++ E-commerce/middleware/validationMiddleware.js
@@ -2,7 +2,8 @@ import {AppError} from '../../E-commerce/utils/asyncErrorHandler.js'
 export const validate=(schema)=>{
     return(req,res,next)=>{
     const{error}=schema.validate({
-        body:req.body
+        body:req.body,
+        file:req.file
     },{abortEarly: false})
     if(error){
         throw new AppError(error,400)
