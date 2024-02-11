236cb2cfeb1b12f776966d1bcc18e1e347fa552f
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-11 22:09:27 +0200

    Update categoryValidation.js

--- E-commerce/validations/categoryValidation.js
+++ E-commerce/validations/categoryValidation.js
@@ -3,11 +3,19 @@ export const addCategorySchema=joi.object({
     body:{
         name:joi.string().trim().max(30).min(1).required(),
         categoryImage:joi.string().hex().length(24)
-    }
+    },
+    params:{
+        categorySlug:joi.string().required()
+    },
+    file:joi.object().required()
 })
 export const updateCategorySchema=joi.object({
     body:{
         name:joi.string().trim().max(30).min(1).required(),
         categoryImage:joi.string().hex().length(24)
-    }
+    },
+    params:{
+        categorySlug:joi.string().required()
+    },
+    file:joi.object()
 })
