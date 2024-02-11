132128567b76d865735b09cec73b766a3b6f130d
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-11 22:46:47 +0200

    Update categoryRouter.js

--- E-commerce/src/modules/category/routes/categoryRouter.js
+++ E-commerce/src/modules/category/routes/categoryRouter.js
@@ -9,13 +9,15 @@ import {validate} from'../../../../middleware/validationMiddleware.js'
 import { addCategorySchema,updateCategorySchema } from "../../../../validations/categoryValidation.js";
 import { categoryModel } from "../model/categoryModel.js";
 import { subCatRouter } from "../../subcategory/routes/subcatRouter.js";
+import { file } from "../../../../middleware/multermiddleware.js";
+import { upload } from "../../../../middleware/imageUploadMiddleware.js";
 export const categoryRouter=Router()
 categoryRouter.route('/')
-.post(validate(addCategorySchema),attachAddQuery(categoryModel),queryExecution())
+.post(file,validate(addCategorySchema),upload({modelImage:'categoryImage'}),attachAddQuery(categoryModel),queryExecution())
 .get(attachFindQuery(categoryModel),queryExecution())
 categoryRouter.route('/:categorySlug')
-.post(validate(addCategorySchema),attachAddQuery(categoryModel),queryExecution())
+.post(file,validate(addCategorySchema),upload({modelImage:'categoryImage'}),attachAddQuery(categoryModel),queryExecution())
 .get(attachFindQuery(categoryModel),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
 .delete(attachDeleteQuery(categoryModel),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
-.put(validate(updateCategorySchema),attachUpdateQuery(categoryModel),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
+.put(file,validate(updateCategorySchema),upload({modelImage:'categoryImage'}),attachUpdateQuery(categoryModel),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
 categoryRouter.use('/:categorySlug/subCategory',subCatRouter)
