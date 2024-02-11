56e77e66785f0b03fdb5580cbdfb600d539b1929
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-11 22:51:55 +0200

    Update subcateValidation.js

--- E-commerce/validations/subcateValidation.js
+++ E-commerce/validations/subcateValidation.js
@@ -4,12 +4,22 @@ export const addsubCategorySchema=joi.object({
         name:joi.string().max(30).min(1).trim().required(),
         category:joi.string().hex().length(24),
         subCatImage:joi.string().hex().length(24)
-    }
+    },
+    params:{
+        categorySlug:joi.string().required(),
+        subcategorySlug:joi.string().required()
+    },
+    file:joi.object().required()
 })
 export const updateSubCatSchema=joi.object({
     body:{
         name:joi.string().max(30).min(1).trim().required(),
         category:joi.string().hex().length(24),
         subCatImage:joi.string().hex().length(24)
-    }
+    },
+    params:{
+        categorySlug:joi.string().required(),
+        subcategorySlug:joi.string().required()
+    },
+    file:joi.object()
 })
