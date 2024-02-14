24c283fc12fa9c4ea6a36afdffea957a374380da
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-15 00:54:44 +0200

    Update brandRouter.js

--- E-commerce/src/modules/brand/routes/brandRouter.js
+++ E-commerce/src/modules/brand/routes/brandRouter.js
@@ -1,14 +1,16 @@
 import {Router} from'express'
-import { attachAddQuery,attachDeleteQuery,attachFindQuery,attachUpdateQuery } from '../../../../middleware/attachedQuery.js'
-import { filterQuery } from '../../../../middleware/filterQuery.js'
-import { queryExecution } from '../../../../middleware/execQuery.js'
 import {validate} from'../../../../middleware/validationMiddleware.js'
 import {addBrandSchema,updateBrandScehma} from '../../../../validations/brandValidationSchema.js'
-import  brandModel  from '../model/brandModel.js'
+import { file } from '../../../../middleware/multermiddleware.js'
+import { upload } from '../../../../middleware/imageUploadMiddleware.js'
+import { addBrand,getBrands,getBrand,updateBrand,deleteBrand } from '../controller/brandController.js'
+import { updateCheck } from '../middleware/checkUpdate.js'
 const brandRouter=Router()
 brandRouter.route('/')
-.post(validate(addBrandSchema),attachAddQuery(brandModel),queryExecution())
-.get(attachFindQuery(brandModel),queryExecution)
-brandRouter.route('/:id').put(validate(updateBrandScehma),attachUpdateQuery(brandModel),filterQuery(),queryExecution())
-.delete(attachDeleteQuery(brandModel),filterQuery(),queryExecution())
+.post(file.single('image'),validate(addBrandSchema),upload({modelImage:'brandImage'}),addBrand)
+.get(getBrands)
+brandRouter.route('/:brandSlug')
+.get(getBrand)
+.put(file.single('image'),validate(updateBrandScehma),updateCheck,upload({modelImage:'brandImage'}),updateBrand)
+.delete(deleteBrand)
 export default brandRouter
