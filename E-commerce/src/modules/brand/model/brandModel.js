60a53942b105be864736a8a84da7e7391619bf18
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-15 00:54:02 +0200

    Update brandModel.js

--- E-commerce/src/modules/brand/model/brandModel.js
+++ E-commerce/src/modules/brand/model/brandModel.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import slugify from "slugify"
+import { imageModel } from "../../../../imageModel.js"
 const brandSchema= new mongoose.Schema({
     title:{
         type:String,
@@ -9,7 +11,29 @@ const brandSchema= new mongoose.Schema({
         type:mongoose.Schema.ObjectId,
         ref:'image',
         unique:true
+    },
+    slug:{
+        type:String,
+        unique:true,
     }
+})
+brandSchema.pre('save',function(next){
+    this.slug=slugify(this.title)
+    next()
+})
+brandSchema.pre(/update/i,async function(next){
+    if(this._update.title){
+        this._update.slug=slugify(this._update.title)
+    }
+    if(this._update.brandImage){
+        const brandImageToBeDeleted=await brandModel.findOne(this._conditions)
+        await imageModel.findByIdAndDelete(brandImageToBeDeleted.brandImage)
+    }
+    next()
+})
+brandSchema.pre(/delete/i,async function(next){
+    const brandImageToBeDeleted=await brandModel.findOne(this._conditions)
+    await imageModel.findByIdAndDelete(brandImageToBeDeleted.brandImage)
 })
  const brandModel=mongoose.model('brand',brandSchema)
  export default brandModel
