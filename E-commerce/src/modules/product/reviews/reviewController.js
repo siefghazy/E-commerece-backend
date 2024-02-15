7fffe1286aed16bb93a4c6a6eff425577962f26a
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-15 16:34:46 +0200

    Update reviewController.js

--- E-commerce/src/modules/product/reviews/reviewController.js
+++ E-commerce/src/modules/product/reviews/reviewController.js
@@ -19,3 +19,19 @@ export const getReviews=catchAsyncError(async(req,res,next)=>{
     const data= await features.query
     res.status(200).json(data)
 })
+export const updateReview=catchAsyncError(async(req,res,next)=>{
+    const{productSlug}=req.params
+    const token=req.header('token')
+    const{_id:userID}=jwt.decode(token)
+    const{_id:productID}=await productModel.findOne({slug:productSlug})
+    await reviewModel.findOneAndUpdate({userID,productID},req.body)
+    res.status(200).json({message:"data updated succ.."})
+})
+export const deleteReview=catchAsyncError(async(req,res,next)=>{
+    const token=req.header('token')
+    const{_id:userID}=jwt.decode(token)
+    const{productSlug}=req.params
+    const{_id:productID}=await productModel.findOne({slug:productSlug})
+    await reviewModel.findOneAndDelete({userID,productID})
+    res.status(200).json({message:"review deleted"})
+})
