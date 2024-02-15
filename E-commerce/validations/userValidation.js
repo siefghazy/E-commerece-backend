41402ebb757dee87f4e336cefd8dfa9aeff984db
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-15 02:56:02 +0200

    Update userValidation.js

--- E-commerce/validations/userValidation.js
+++ E-commerce/validations/userValidation.js
@@ -13,3 +13,16 @@ export const updateUserSchema=joi.object({
         role:joi.string().required()
     }
 })
+export const signUpSchema=joi.object({
+    body:{
+        name:joi.string().max(30).min(3).required(),
+        password:joi.string().max(100).min(5).required(),
+        role:joi.string().required()
+    }
+})
+export const signinScehma=joi.object({
+    body:{
+        name:joi.string().max(30).min(3).required(),
+        password:joi.string().max(100).min(5).required(),
+    }
+})
