0dab7a5e40c57bed7416b4ddedb1d736433d5902
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-15 17:24:23 +0200

    Update categoryRouter.js

--- E-commerce/src/modules/category/routes/categoryRouter.js
+++ E-commerce/src/modules/category/routes/categoryRouter.js
@@ -11,14 +11,15 @@ import { categoryModel } from "../model/categoryModel.js";
 import { subCatRouter } from "../../subcategory/routes/subcatRouter.js";
 import { file } from "../../../../middleware/multermiddleware.js";
 import { upload } from "../../../../middleware/imageUploadMiddleware.js";
-import { deleteImage } from "../../../../middleware/imageUploadMiddleware.js";
+import { auth } from "../../../../middleware/auth.js";
+import { authorization } from "../../../../middleware/authorization.js";
 export const categoryRouter=Router()
 categoryRouter.route('/')
-.post(file,validate(addCategorySchema),upload({modelImage:'categoryImage'}),attachAddQuery(categoryModel),queryExecution())
+.post(auth,authorization,file.single('img'),validate(addCategorySchema),upload({modelImage:'categoryImage'}),attachAddQuery(categoryModel),queryExecution())
 .get(attachFindQuery(categoryModel),queryExecution())
 categoryRouter.route('/:categorySlug')
-.post(file,validate(addCategorySchema),upload({modelImage:'categoryImage'}),attachAddQuery(categoryModel),queryExecution())
+.post(auth,authorization,file.single('img'),validate(addCategorySchema),upload({modelImage:'categoryImage'}),attachAddQuery(categoryModel),queryExecution())
 .get(attachFindQuery(categoryModel),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
-.delete(attachDeleteQuery(categoryModel),deleteImage(),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
-.put(file,validate(updateCategorySchema),upload({modelImage:'categoryImage'}),attachUpdateQuery(categoryModel),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
+.delete(auth,authorization,attachDeleteQuery(categoryModel),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
+.put(auth,authorization,file.single('img'),validate(updateCategorySchema),upload({modelImage:'categoryImage'}),attachUpdateQuery(categoryModel),filterQuery({fieldName:'slug',paramName:'categorySlug'}),queryExecution())
 categoryRouter.use('/:categorySlug/subCategory',subCatRouter)
