d70738f70cfad4df08b2f5a5432cac333b0608a4
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-16 10:23:29 +0200

    Update auth.js

--- E-commerce/src/modules/auth/auth.js
+++ E-commerce/src/modules/auth/auth.js
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken'
 import env from "dotenv"
 import bcrypt from 'bcrypt'
-import { userModel } from '../user/models/userModel.js';
+import { userModel } from '../user/model/userModel.js';
 import { catchAsyncError,AppError } from "../../../utils/asyncErrorHandler.js";
+import { transporter } from '../../../middleware/nodemailerMiddleware.js';
 env.config()
 export const signUp=catchAsyncError(async(req,res,next)=>{
     const{email}=req.body
@@ -10,7 +11,16 @@ export const signUp=catchAsyncError(async(req,res,next)=>{
     if(!data){
     const{email,password,role}=req.body
     const hashedPassword=bcrypt.hashSync(password,parseInt(process.env.SALT_NUMBER))
-    await userModel.create({email,password:hashedPassword})
+    await userModel.create({email,password:hashedPassword,role})
+    const emailToken=Jwt.sign({email},process.env.EMAIL_SECRET_KEY)
+    const link=process.env.HOST+`confirmemail/${emailToken}`
+    await transporter.sendMail({
+      from:process.env.EMAIL,
+      to:email,
+      subject:"Email confirmation",
+      text:"confirm your email",
+      html: `<a href=${link}>click here to verify the email</a>`
+    })
     return res.status(200).json({message:'sign up succ..'})
     }
     throw new AppError('email already exist',400)
