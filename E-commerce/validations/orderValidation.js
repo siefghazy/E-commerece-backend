e46a3dba734c66479a09dcd87235cf029e403d44
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-22 09:03:02 +0200

    Update orderValidation.js

--- E-commerce/validations/orderValidation.js
+++ E-commerce/validations/orderValidation.js
@@ -1,18 +1,8 @@
 import joi from 'joi'
 export const addOrderSchema=joi.object({
     body:{
-        userID:joi.string().hex().length(24).required(),
-        products:joi.array().items(joi.object({
-            proudctID:joi.string().hex().length(24),
-            quantity:joi.number().min(1)
-        }))
-    }
-})
-export const updateOrderSchema=joi.object({
-    body:{
-        products:joi.array().items(joi.object({
-            proudctID:joi.string().hex().length(24),
-            quantity:joi.number().min(1)
-        }))
-    }
+        phoneNumber:joi.string().required().pattern(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/),
+        address:joi.string()
+    },
+    params:{}
 })
