9d726fe3d9039ea61bb5ff1e40352c63bda87975
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-22 09:02:35 +0200

    Update cartValidation.js

--- E-commerce/validations/cartValidation.js
+++ E-commerce/validations/cartValidation.js
@@ -1,19 +1,7 @@
 import joi from'joi'
 export const addCartSchema=joi.object({
     body:{
-        userID:joi.string().hex().length(24).required(),
-        ordersOncart:joi.array().items(joi.object({
-            productID:joi.string().hex().length(24).required(),
-            quantity:joi.string().hex().length(24).required()
-        }))
-    }
-})
-export const updateCartSchema=joi.object({
-    body:{
-        userID:joi.string().hex().length(24).required(),
-        ordersOncart:joi.array().items(joi.object({
-            productID:joi.string().hex().length(24).required(),
-            quantity:joi.string().hex().length(24).required()
-        }))
-    }
+        productID:joi.string().length(24).hex().required()
+    },
+    params:{}
 })
