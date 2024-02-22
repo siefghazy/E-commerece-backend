f89b48a9d100ee3d14510c467312d6371dfbd537
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-22 08:59:09 +0200

    Update cartModel.js

--- E-commerce/src/modules/cart/models/cartModel.js
+++ E-commerce/src/modules/cart/models/cartModel.js
@@ -19,5 +19,30 @@ const cartSchema=new mongoose.Schema({
             }
         }
     ],
-},{timestamps:true})
+    coupon:{
+        type:mongoose.Schema.ObjectId,
+        ref:'coupon'
+    }
+},{
+    timestamps:true,
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+})
+cartSchema.pre(/find/i,function(next){
+    this.populate({
+        path:'ordersOnCart',
+        populate:{
+            path:'productID',
+            model:'product'
+        }
+    })
+    this.populate('coupon')
+    next()
+})
+cartSchema.virtual("totalPrice").get(function(){
+    const total=this.ordersOnCart.reduce(
+        (acc,entery)=>acc+entery.productID.price*entery.quantity
+    ,0)
+    return total-((this.coupon?.discount||0)/100)*total
+})
 export const cartModel=mongoose.model('cart',cartSchema)
