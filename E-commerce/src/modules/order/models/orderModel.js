beeae43ee4f1c0d1ef4c3b699846709ce11850ba
siefghazy <133509228+siefghazy@users.noreply.github.com>
2024-02-22 08:59:47 +0200

    Update orderModel.js

--- E-commerce/src/modules/order/models/orderModel.js
+++ E-commerce/src/modules/order/models/orderModel.js
@@ -9,15 +9,42 @@ const orderSchema=new mongoose.Schema({
     products:
     [
     {
-        productID:{
-            type:mongoose.Schema.ObjectId,
-            ref:'product'
+        product:{
+            title:String,
+            price:Number,
         },
         quantity:{
             type:Number,
-            min:1
+            required:true
         }
     }
-    ]
+    ],
+    coupon:{
+        discount:Number
+    },
+    phoneNumber:String,
+    address:String,
+    paymentType:{
+        type:String,
+        enum:['COD','CARD'],
+        default:'COD'
+    },
+    is_delivered:{
+        type:Boolean,
+        default:false
+    },
+    is_paid:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true,
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+})
+orderSchema.virtual('totalPrice').get(function(){
+    const total =this.products.reduce((acc,entery)=>{
+        return acc+entery.product.price*entery.quantity
+    },0)
+     return total-((this.coupon?.discount||0)/100)*total
 })
 export const orderModel=mongoose.model('order',orderSchema)
